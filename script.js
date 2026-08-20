8a97f89932caae81a2de11327d3ba3873829f006
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-08-20 19:20:38 -0300

    commit produtos
    array produtos criada

---
+++ script.js
@@ -0,0 +1,18 @@
+const produtos = [
+    { 
+        categoria : 'Camisetas',
+        img : 'imagens/camisetas/branco.png',
+        nome : 'Camiseta Branca',
+        preco : 49.9,
+        tamanho : ['PP','P','M','G','GG']
+    }, 
+    {
+        categoria : 'Camisetas',
+        img : 'imagens/camisetas/azul.png',
+        nome : 'Camiseta Azul',
+        preco : 49.9,
+        tamanho : ['PP','P','M','G','GG']
+    }
+]
+
+
