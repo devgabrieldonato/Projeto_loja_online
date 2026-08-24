af4ba6feee1ccb31822b060f29ce9a4cb028564e
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-08-24 19:47:43 -0300

    Novas atualizações
    adicionei id nos objetos,  coloquei id unico para cada tamanho de botão, e comecei a implementar a questão de quando usuário apertar no tamanho ficar selecionado.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const produtos = [
   {
+    id: 1,
     categoria: 'Camiseta',
     nome: 'Camiseta Branca',
     imagem: 'imagens/camisetas/branco.png',
@@ -7,6 +8,7 @@ const produtos = [
     tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
   },
   {
+    id: 2,
     categoria: 'Camiseta',
     nome: 'Camiseta Azul',
     imagem: 'imagens/camisetas/azul.png',
@@ -14,6 +16,7 @@ const produtos = [
     tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
   },
   {
+    id: 3,
     categoria: 'Moletom',
     nome: 'Moletom Branco',
     imagem: 'imagens/blusas-moletom/branco.png',
@@ -21,9 +24,18 @@ const produtos = [
     tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
   },
   {
+    id: 4,
     categoria: 'Moletom',
     nome: 'Moletom Azul',
     imagem: 'imagens/blusas-moletom/azul.png',
+    preco: 49.9,
+    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+  },
+  {
+    id: 5,
+    categoria: 'Calças',
+    nome: 'Calça Branco',
+    imagem: 'imagens/calcas-moletom/branco.png',
     preco: 49.9,
     tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
   },
@@ -35,19 +47,18 @@ document.addEventListener('DOMContentLoaded', () => {
     ...new Set(produtos.map((produto2) => produto2.categoria)),
   ];
 
-  let contador_div = 1;
-  let contador_section = 1;
+  let contador = 1;
   categorias.forEach((categoria) => {
-    let incremento = `produtos${contador_div}`;
+    let incremento = `produtos${contador}`;
     const sectionCategoria = document.createElement('section');
     sectionCategoria.classList.add('categorias');
-    sectionCategoria.id = contador_section;
+    sectionCategoria.id = contador;
     main.append(sectionCategoria);
-    document.getElementById(contador_section).innerHTML +=
-      `<h2>${categoria}</h2>`;
+    document.getElementById(contador).innerHTML += `<h2>${categoria}</h2>`;
     const divProdutos = document.createElement('div');
-    divProdutos.classList.add(incremento);
-    document.querySelector('.categorias').append(divProdutos);
+    divProdutos.classList.add('produtos');
+    divProdutos.id = incremento;
+    document.getElementById(contador).append(divProdutos);
 
     produtos.forEach((produto) => {
       if (produto.categoria === categoria) {
@@ -56,23 +67,35 @@ document.addEventListener('DOMContentLoaded', () => {
         let botoesTamanho = ''; // variável para armazenar os botões de tamanho
 
         produto.tamanhos.forEach((tamanho) => {
-          botoesTamanho += `<button>${tamanho}</button>`;
+          botoesTamanho += `<button id='${produto.id}_${tamanho}' class='buttonTamanho'>${tamanho}</button>`;
+          let id = `${produto.id}_${tamanho}`;
         });
         // adicionado o dado-nome e dado-preco para caso queira pegar no carrinho, e adicionado o alt na imagem para acessibilidade.
-        document.querySelector(`.${incremento}`).innerHTML +=
-          `<article dado-nome=${produto.nome} dado-preco=${produto.preco}> 
+        document.querySelector(`#${incremento}`).innerHTML +=
+          `<article dado-nome=${produto.id} dado-preco=${produto.preco}> 
                     <img class="imagem-produto" src="${produto.imagem}" alt="${produto.nome}"> 
                     <h3>${produto.nome}</h3>
                     <p>R$ ${String(produto.preco.toFixed(2)).replace('.', ',')}</p>
                     <p>Selecione o tamanho</p>
-                    <div>
+                    <div id='conjuntoBotoes_${produto.id}'>
                         ${botoesTamanho}
                     </div>
                     <button>Adicionar ao carrinho</button>
                     </article>`;
-        contador_div = contador_div + 1;
+
+        const conjunto = document.getElementById(
+          `conjuntoBotoes_${produto.id}`,
+        );
+        conjunto.addEventListener('click', (e) => {
+          const button = e.target.closest('.buttonTamanho');
+
+          if (button) {
+            const idButton = button.id;
+            console.log(idButton);
+          }
+        });
       }
     });
-    contador_section = +1;
+    contador = contador + 1;
   });
 });
