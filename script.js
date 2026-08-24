876bac12784a8f7301edeb552abc88335b4b61f5
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-08-24 13:33:19 -0300

    tentando arrumar a semantica que é criado pelo JS
    Tentado deixar a semâmtica do Index2 a mesma do index 1, via JS

--- script.js
+++ script.js
@@ -1,81 +1,78 @@
 const produtos = [
-         {      
-            categoria : 'Camiseta',
-            nome: 'Camiseta Branca',
-            imagem: 'imagens/camisetas/branco.png',
-            preco: 49.90,
-            tamanhos: ['PP', 'P', 'M', 'G', 'GG']
-        },
-        {
-            categoria : 'Camiseta',
-            nome: 'Camiseta Azul',
-            imagem: 'imagens/camisetas/azul.png',
-            preco: 49.90,
-            tamanhos: ['PP', 'P', 'M', 'G', 'GG']
-        },
-        {
-            categoria : 'Moletom',
-            nome: 'Moletom Branco',
-            imagem: 'imagens/blusas-moletom/branco.png',
-            preco: 49.90,
-            tamanhos: ['PP', 'P', 'M', 'G', 'GG']
-        },
-        {
-            categoria : 'Moletom',
-            nome: 'Moletom Azul',
-            imagem: 'imagens/blusas-moletom/azul.png',
-            preco: 49.90,            tamanhos: ['PP', 'P', 'M', 'G', 'GG']
-        }
-]
-const categoria_list = [];
+  {
+    categoria: 'Camiseta',
+    nome: 'Camiseta Branca',
+    imagem: 'imagens/camisetas/branco.png',
+    preco: 49.9,
+    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+  },
+  {
+    categoria: 'Camiseta',
+    nome: 'Camiseta Azul',
+    imagem: 'imagens/camisetas/azul.png',
+    preco: 49.9,
+    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+  },
+  {
+    categoria: 'Moletom',
+    nome: 'Moletom Branco',
+    imagem: 'imagens/blusas-moletom/branco.png',
+    preco: 49.9,
+    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+  },
+  {
+    categoria: 'Moletom',
+    nome: 'Moletom Azul',
+    imagem: 'imagens/blusas-moletom/azul.png',
+    preco: 49.9,
+    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+  },
+];
 const main = document.querySelector('main');
 
-document.addEventListener("DOMContentLoaded",()=>{
+document.addEventListener('DOMContentLoaded', () => {
+  const categorias = [
+    ...new Set(produtos.map((produto2) => produto2.categoria)),
+  ];
 
-    let tamanho = produtos.length;
-    
-    const categorias = [...new Set(produtos.map((produto) => produto.categoria))];
-    
-    categorias.forEach(categoria =>{
-        main.innerHTML += `<h2>${categoria}</h2>`
-        produtos.forEach(produto2 => {
-            if (produto2.categoria === categoria) { // compara a categoria do produto com a categoria atual do loop. adicionado o ===;
-                
-               /*  document.querySelector('main').innerHTML += `<article dado-nome=${produto2.nome} dado-preco=${produto2.preco}> 
-                    <img class="imagem-produto" src="${produto2.imagem}" alt="${produto2.nome}"> 
-                    <h3>${produto2.nome}</h3>
-                    <p>R$ ${(String((produto2.preco).toFixed(2))).replace('.',',')}</p>
-                    <p>Selecione o tamanho</p>
-                    <div>` 
-                    comentei todo esse bloco aqui não quis apagar para deixar visual como estava antes, quiser apagar pode apagar, mas foi mais para entender como que tava e como que ficou
-                    o mesmo  serve para o bloco de baixo */
+  let contador_div = 1;
+  let contador_section = 1;
+  categorias.forEach((categoria) => {
+    let incremento = `produtos${contador_div}`;
+    const sectionCategoria = document.createElement('section');
+    sectionCategoria.classList.add('categorias');
+    sectionCategoria.id = contador_section;
+    main.append(sectionCategoria);
+    document.getElementById(contador_section).innerHTML +=
+      `<h2>${categoria}</h2>`;
+    const divProdutos = document.createElement('div');
+    divProdutos.classList.add(incremento);
+    document.querySelector('.categorias').append(divProdutos);
 
-                    let botoesTamanho = ''; // variável para armazenar os botões de tamanho
+    produtos.forEach((produto) => {
+      if (produto.categoria === categoria) {
+        // compara a categoria do produto com a categoria atual do loop. adicionado o ===;
 
-                    produto2.tamanhos.forEach(tamanho => {
-                        botoesTamanho += `<button>${tamanho}</button>`
-                    });
-                    // adicionado o dado-nome e dado-preco para caso queira pegar no carrinho, e adicionado o alt na imagem para acessibilidade.
-                    document.querySelector('main').innerHTML += 
-                    `<article dado-nome=${produto2.nome} dado-preco=${produto2.preco}> 
-                    <img class="imagem-produto" src="${produto2.imagem}" alt="${produto2.nome}"> 
-                    <h3>${produto2.nome}</h3>
-                    <p>R$ ${(String((produto2.preco).toFixed(2))).replace('.',',')}</p>
+        let botoesTamanho = ''; // variável para armazenar os botões de tamanho
+
+        produto.tamanhos.forEach((tamanho) => {
+          botoesTamanho += `<button>${tamanho}</button>`;
+        });
+        // adicionado o dado-nome e dado-preco para caso queira pegar no carrinho, e adicionado o alt na imagem para acessibilidade.
+        document.querySelector(`.${incremento}`).innerHTML +=
+          `<article dado-nome=${produto.nome} dado-preco=${produto.preco}> 
+                    <img class="imagem-produto" src="${produto.imagem}" alt="${produto.nome}"> 
+                    <h3>${produto.nome}</h3>
+                    <p>R$ ${String(produto.preco.toFixed(2)).replace('.', ',')}</p>
                     <p>Selecione o tamanho</p>
                     <div>
                         ${botoesTamanho}
                     </div>
                     <button>Adicionar ao carrinho</button>
-                    </article>`
-
-                   /*  document.querySelector('main').innerHTML += `
-                    </div>
-                    <button>Adicionar ao carrinho</button>
-                </article>` */
-                    
-            } 
-            
-        });
-    })
-  
-})
+                    </article>`;
+        contador_div = contador_div + 1;
+      }
+    });
+    contador_section = +1;
+  });
+});
