d67ecc1aa1da36a581d63e95b07da2197ab6f8ee
Jorge Augusto Flores Meert <jorgeflmrttt@icloud.com>
2026-08-31 22:21:11 -0300

    MVP pronto

--- script.js
+++ script.js
@@ -2,366 +2,368 @@ const produtos = [
   // Camisetas
   {
     id: 1,
-    categoria: 'Camiseta',
-    nome: 'Camiseta Branca',
-    imagem: 'imagens/camisetas/branco.png',
+    categoria: "Camiseta",
+    nome: "Camiseta Branca",
+    imagem: "imagens/camisetas/branco.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 2,
-    categoria: 'Camiseta',
-    nome: 'Camiseta Azul',
-    imagem: 'imagens/camisetas/azul.png',
+    categoria: "Camiseta",
+    nome: "Camiseta Azul",
+    imagem: "imagens/camisetas/azul.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 3,
-    categoria: 'Camiseta',
-    nome: 'Camiseta Cinza',
-    imagem: 'imagens/camisetas/cinza.png',
+    categoria: "Camiseta",
+    nome: "Camiseta Cinza",
+    imagem: "imagens/camisetas/cinza.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 4,
-    categoria: 'Camiseta',
-    nome: 'Camiseta Preta',
-    imagem: 'imagens/camisetas/preto.png',
+    categoria: "Camiseta",
+    nome: "Camiseta Preta",
+    imagem: "imagens/camisetas/preto.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 5,
-    categoria: 'Camiseta',
-    nome: 'Camiseta Verde',
-    imagem: 'imagens/camisetas/verde.png',
+    categoria: "Camiseta",
+    nome: "Camiseta Verde",
+    imagem: "imagens/camisetas/verde.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 6,
-    categoria: 'Camiseta',
-    nome: 'Camiseta Vermelha',
-    imagem: 'imagens/camisetas/vermelho.png',
+    categoria: "Camiseta",
+    nome: "Camiseta Vermelha",
+    imagem: "imagens/camisetas/vermelho.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 
   // Casacos de Moletom
   {
     id: 7,
-    categoria: 'Moletom',
-    nome: 'Moletom Branco',
-    imagem: 'imagens/blusas-moletom/branco.png',
+    categoria: "Moletom",
+    nome: "Moletom Branco",
+    imagem: "imagens/blusas-moletom/branco.png",
     preco: 129.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 8,
-    categoria: 'Moletom',
-    nome: 'Moletom Azul',
-    imagem: 'imagens/blusas-moletom/azul.png',
+    categoria: "Moletom",
+    nome: "Moletom Azul",
+    imagem: "imagens/blusas-moletom/azul.png",
     preco: 129.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 9,
-    categoria: 'Moletom',
-    nome: 'Moletom Cinza',
-    imagem: 'imagens/blusas-moletom/cinza.png',
+    categoria: "Moletom",
+    nome: "Moletom Cinza",
+    imagem: "imagens/blusas-moletom/cinza.png",
     preco: 129.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 10,
-    categoria: 'Moletom',
-    nome: 'Moletom Preto',
-    imagem: 'imagens/blusas-moletom/preto.png',
+    categoria: "Moletom",
+    nome: "Moletom Preto",
+    imagem: "imagens/blusas-moletom/preto.png",
     preco: 129.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 11,
-    categoria: 'Moletom',
-    nome: 'Moletom Verde',
-    imagem: 'imagens/blusas-moletom/verde.png',
+    categoria: "Moletom",
+    nome: "Moletom Verde",
+    imagem: "imagens/blusas-moletom/verde.png",
     preco: 129.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 12,
-    categoria: 'Moletom',
-    nome: 'Moletom Vermelho',
-    imagem: 'imagens/blusas-moletom/vermelho.png',
+    categoria: "Moletom",
+    nome: "Moletom Vermelho",
+    imagem: "imagens/blusas-moletom/vermelho.png",
     preco: 129.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 
   // Bermudas
   {
     id: 13,
-    categoria: 'Bermuda',
-    nome: 'Bermuda Branca',
-    imagem: 'imagens/bermudas/branco.png',
+    categoria: "Bermuda",
+    nome: "Bermuda Branca",
+    imagem: "imagens/bermudas/branco.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 14,
-    categoria: 'Bermuda',
-    nome: 'Bermuda Azul',
-    imagem: 'imagens/bermudas/azul.png',
+    categoria: "Bermuda",
+    nome: "Bermuda Azul",
+    imagem: "imagens/bermudas/azul.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 15,
-    categoria: 'Bermuda',
-    nome: 'Bermuda Cinza',
-    imagem: 'imagens/bermudas/cinza.png',
+    categoria: "Bermuda",
+    nome: "Bermuda Cinza",
+    imagem: "imagens/bermudas/cinza.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 16,
-    categoria: 'Bermuda',
-    nome: 'Bermuda Preta',
-    imagem: 'imagens/bermudas/preto.png',
+    categoria: "Bermuda",
+    nome: "Bermuda Preta",
+    imagem: "imagens/bermudas/preto.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 17,
-    categoria: 'Bermuda',
-    nome: 'Bermuda Verde',
-    imagem: 'imagens/bermudas/verde.png',
+    categoria: "Bermuda",
+    nome: "Bermuda Verde",
+    imagem: "imagens/bermudas/verde.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 18,
-    categoria: 'Bermuda',
-    nome: 'Bermuda Vermelha',
-    imagem: 'imagens/bermudas/vermelho.png',
+    categoria: "Bermuda",
+    nome: "Bermuda Vermelha",
+    imagem: "imagens/bermudas/vermelho.png",
     preco: 49.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 
   // Calças de Moletom
   {
     id: 19,
-    categoria: 'CalçaMoletom',
-    nome: 'Calça Moletom Branca',
-    imagem: 'imagens/calcas-moletom/branco.png',
+    categoria: "Calça Moletom",
+    nome: "Calça Moletom Branca",
+    imagem: "imagens/calcas-moletom/branco.png",
     preco: 99.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 20,
-    categoria: 'CalçaMoletom',
-    nome: 'Calça Moletom Azul',
-    imagem: 'imagens/calcas-moletom/azul.png',
+    categoria: "Calça Moletom",
+    nome: "Calça Moletom Azul",
+    imagem: "imagens/calcas-moletom/azul.png",
     preco: 99.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 21,
-    categoria: 'CalçaMoletom',
-    nome: 'Calça Moletom Cinza',
-    imagem: 'imagens/calcas-moletom/cinza.png',
+    categoria: "Calça Moletom",
+    nome: "Calça Moletom Cinza",
+    imagem: "imagens/calcas-moletom/cinza.png",
     preco: 99.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 22,
-    categoria: 'CalçaMoletom',
-    nome: 'Calça Moletom Preta',
-    imagem: 'imagens/calcas-moletom/preto.png',
+    categoria: "Calça Moletom",
+    nome: "Calça Moletom Preta",
+    imagem: "imagens/calcas-moletom/preto.png",
     preco: 99.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 23,
-    categoria: 'CalçaMoletom',
-    nome: 'Calça Moletom Verde',
-    imagem: 'imagens/calcas-moletom/verde.png',
+    categoria: "Calça Moletom",
+    nome: "Calça Moletom Verde",
+    imagem: "imagens/calcas-moletom/verde.png",
     preco: 99.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 24,
-    categoria: 'CalçaMoletom',
-    nome: 'Calça Moletom Vermelha',
-    imagem: 'imagens/calcas-moletom/vermelho.png',
+    categoria: "Calça Moletom",
+    nome: "Calça Moletom Vermelha",
+    imagem: "imagens/calcas-moletom/vermelho.png",
     preco: 99.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 
   // Meias
   {
     id: 25,
-    categoria: 'Meia',
-    nome: 'Meia Branca',
-    imagem: 'imagens/meias/branco.png',
+    categoria: "Meia",
+    nome: "Meia Branca",
+    imagem: "imagens/meias/branco.png",
     preco: 14.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 26,
-    categoria: 'Meia',
-    nome: 'Meia Azul',
-    imagem: 'imagens/meias/azul.png',
+    categoria: "Meia",
+    nome: "Meia Azul",
+    imagem: "imagens/meias/azul.png",
     preco: 14.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 27,
-    categoria: 'Meia',
-    nome: 'Meia Cinza',
-    imagem: 'imagens/meias/cinza.png',
+    categoria: "Meia",
+    nome: "Meia Cinza",
+    imagem: "imagens/meias/cinza.png",
     preco: 14.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 28,
-    categoria: 'Meia',
-    nome: 'Meia Preta',
-    imagem: 'imagens/meias/preto.png',
+    categoria: "Meia",
+    nome: "Meia Preta",
+    imagem: "imagens/meias/preto.png",
     preco: 14.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 29,
-    categoria: 'Meia',
-    nome: 'Meia Verde',
-    imagem: 'imagens/meias/verde.png',
+    categoria: "Meia",
+    nome: "Meia Verde",
+    imagem: "imagens/meias/verde.png",
     preco: 14.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 30,
-    categoria: 'Meia',
-    nome: 'Meia Vermelha',
-    imagem: 'imagens/meias/vermelho.png',
+    categoria: "Meia",
+    nome: "Meia Vermelha",
+    imagem: "imagens/meias/vermelho.png",
     preco: 14.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 
   // Cuecas
   {
     id: 31,
-    categoria: 'Cueca',
-    nome: 'Cueca Branca',
-    imagem: 'imagens/cuecas/branco.png',
+    categoria: "Cueca",
+    nome: "Cueca Branca",
+    imagem: "imagens/cuecas/branco.png",
     preco: 29.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 32,
-    categoria: 'Cueca',
-    nome: 'Cueca Azul',
-    imagem: 'imagens/cuecas/azul.png',
+    categoria: "Cueca",
+    nome: "Cueca Azul",
+    imagem: "imagens/cuecas/azul.png",
     preco: 29.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 33,
-    categoria: 'Cueca',
-    nome: 'Cueca Cinza',
-    imagem: 'imagens/cuecas/cinza.png',
+    categoria: "Cueca",
+    nome: "Cueca Cinza",
+    imagem: "imagens/cuecas/cinza.png",
     preco: 29.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 34,
-    categoria: 'Cueca',
-    nome: 'Cueca Preta',
-    imagem: 'imagens/cuecas/preto.png',
+    categoria: "Cueca",
+    nome: "Cueca Preta",
+    imagem: "imagens/cuecas/preto.png",
     preco: 29.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 35,
-    categoria: 'Cueca',
-    nome: 'Cueca Verde',
-    imagem: 'imagens/cuecas/verde.png',
+    categoria: "Cueca",
+    nome: "Cueca Verde",
+    imagem: "imagens/cuecas/verde.png",
     preco: 29.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 36,
-    categoria: 'Cueca',
-    nome: 'Cueca Vermelha',
-    imagem: 'imagens/cuecas/vermelho.png',
+    categoria: "Cueca",
+    nome: "Cueca Vermelha",
+    imagem: "imagens/cuecas/vermelho.png",
     preco: 29.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 
   // Bonés
   {
     id: 37,
-    categoria: 'Boné',
-    nome: 'Boné Branco',
-    imagem: 'imagens/bones/branco.png',
+    categoria: "Boné",
+    nome: "Boné Branco",
+    imagem: "imagens/bones/branco.png",
     preco: 39.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 38,
-    categoria: 'Boné',
-    nome: 'Boné Azul',
-    imagem: 'imagens/bones/azul.png',
+    categoria: "Boné",
+    nome: "Boné Azul",
+    imagem: "imagens/bones/azul.png",
     preco: 39.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 39,
-    categoria: 'Boné',
-    nome: 'Boné Cinza',
-    imagem: 'imagens/bones/cinza.png',
+    categoria: "Boné",
+    nome: "Boné Cinza",
+    imagem: "imagens/bones/cinza.png",
     preco: 39.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 40,
-    categoria: 'Boné',
-    nome: 'Boné Preto',
-    imagem: 'imagens/bones/preto.png',
+    categoria: "Boné",
+    nome: "Boné Preto",
+    imagem: "imagens/bones/preto.png",
     preco: 39.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 41,
-    categoria: 'Boné',
-    nome: 'Boné Verde',
-    imagem: 'imagens/bones/verde.png',
+    categoria: "Boné",
+    nome: "Boné Verde",
+    imagem: "imagens/bones/verde.png",
     preco: 39.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
   {
     id: 42,
-    categoria: 'Boné',
-    nome: 'Boné Vermelho',
-    imagem: 'imagens/bones/vermelho.png',
+    categoria: "Boné",
+    nome: "Boné Vermelho",
+    imagem: "imagens/bones/vermelho.png",
     preco: 39.9,
-    tamanhos: ['PP', 'P', 'M', 'G', 'GG'],
+    tamanhos: ["PP", "P", "M", "G", "GG"],
   },
 ];
 
-const main = document.querySelector('main');
-const vitrineProdutos = document.getElementById('vitrine-produtos');
+const main = document.querySelector("main");
+const vitrineProdutos = document.getElementById("vitrine-produtos");
 
 function renderizarProdutos(listaProdutos) {
-  vitrineProdutos.querySelectorAll('.categorias, .sem-produtos').forEach((elemento) => {
-    elemento.remove();
-  });
+  vitrineProdutos
+    .querySelectorAll(".categorias, .sem-produtos")
+    .forEach((elemento) => {
+      elemento.remove();
+    });
 
   if (listaProdutos.length === 0) {
-    const mensagem = document.createElement('p');
-    mensagem.classList.add('sem-produtos');
-    mensagem.textContent = 'Nenhum produto encontrado com esses filtros.';
+    const mensagem = document.createElement("p");
+    mensagem.classList.add("sem-produtos");
+    mensagem.textContent = "Nenhum produto encontrado com esses filtros.";
     vitrineProdutos.append(mensagem);
     return;
   }
@@ -377,9 +379,9 @@ function renderizarProdutos(listaProdutos) {
     // incremento para salvar o id depois.
     let incremento = `produtos${contador}`;
     // cria uma section no html
-    const sectionCategoria = document.createElement('section');
+    const sectionCategoria = document.createElement("section");
     // cria uma classe 'categorias'
-    sectionCategoria.classList.add('categorias');
+    sectionCategoria.classList.add("categorias");
     //  da um id pra ela que seria o contador, cada vez que o forEach percorre uma vez, aumenta 1 ( nunca vai ser o mesmo)
     sectionCategoria.id = contador;
     // faz o append ao HTML (adiciona)
@@ -387,9 +389,9 @@ function renderizarProdutos(listaProdutos) {
     // no id que demos antes na section, ele adiciona a Categoria
     document.getElementById(contador).innerHTML += `<h2>${categoria}</h2>`;
     // depois de adicionar a section cria uma div
-    const divProdutos = document.createElement('div');
+    const divProdutos = document.createElement("div");
     // demos uma classe de produtos
-    divProdutos.classList.add('produtos');
+    divProdutos.classList.add("produtos");
     // dou o id do incremento (ex : produtos1, produtos2), div onde vai os articles dos itens, onde vamos adicionar os itens
     divProdutos.id = incremento;
     // fizemos append para o HTML
@@ -399,7 +401,7 @@ function renderizarProdutos(listaProdutos) {
     listaProdutos.forEach((produto) => {
       // quando a categoria do item for igual a categoria
       if (produto.categoria === categoria) {
-        let botoesTamanho = ''; // variável para armazenar os botões de tamanho
+        let botoesTamanho = ""; // variável para armazenar os botões de tamanho
 
         // ForEach para criar a quantidade certa de botão, dependeno de quanto tamanho tem cada produto.
         produto.tamanhos.forEach((tamanho) => {
@@ -410,10 +412,10 @@ function renderizarProdutos(listaProdutos) {
 
         // adicionando itens ao nosso id incremento que demos antes( a div), toda a vez que o for percorrer, ele irá adicionar ao html os itens abaixo.
         document.querySelector(`#${incremento}`).innerHTML +=
-          `<article dado-nome=${produto.id} dado-preco=${produto.preco}> 
-                    <img class="imagem-produto" src="${produto.imagem}" alt="${produto.nome}"> 
+          `<article dado-nome=${produto.id} dado-preco=${produto.preco}>
+                    <img class="imagem-produto" src="${produto.imagem}" alt="${produto.nome}">
                     <h3>${produto.nome}</h3>
-                    <p>R$ ${String(produto.preco.toFixed(2)).replace('.', ',')}</p>
+                    <p>R$ ${String(produto.preco.toFixed(2)).replace(".", ",")}</p>
                     <p>Selecione o tamanho</p>
                     <div id='conjuntoBotoes_${produto.id}'>
                         ${botoesTamanho}
@@ -428,25 +430,25 @@ function renderizarProdutos(listaProdutos) {
 }
 
 // Evento para acontecer quando á pagina for carregada
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   renderizarProdutos(produtos);
 
   // event para o usuário escolher um tamanho.
-  main.addEventListener('click', (e) => {
+  main.addEventListener("click", (e) => {
     const idButton = e.target.id;
     const button = document.getElementById(idButton);
     // caso haja mais de um tamanho escolhido ele procura todos
-    const botoesEscolhidos = document.querySelectorAll('.tamanhoEscolhido');
+    const botoesEscolhidos = document.querySelectorAll(".tamanhoEscolhido");
     // se tiver ele vai percorrer esse forEach e remover a classe tamanhoEscolhido, para o usuário escolher apenas 1
     botoesEscolhidos.forEach((botao) => {
-      botao.classList.remove('tamanhoEscolhido');
+      botao.classList.remove("tamanhoEscolhido");
     });
     // verificando se o usuário clicou mesmo na classe 'buttonTamanho', onde está localizada em todos os botões de tamanho.
-    if (e.target.closest('.buttonTamanho')) {
-      if (button.classList.contains('tamanhoEscolhido')) {
-        button.classList.remove('tamanhoEscolhido');
+    if (e.target.closest(".buttonTamanho")) {
+      if (button.classList.contains("tamanhoEscolhido")) {
+        button.classList.remove("tamanhoEscolhido");
       } else {
-        button.classList.add('tamanhoEscolhido');
+        button.classList.add("tamanhoEscolhido");
       }
     }
   });
